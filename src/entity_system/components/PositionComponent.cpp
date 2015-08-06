40d0687ae807f0504bad9db8707601e3712d5619
Komodoman <the.big.kb123@gmail.com>
2015-08-05 22:46:15 -0400

    Deleted AiComponent - starting with something more simple - PositionComponent

---
+++ src/entity_system/components/PositionComponent.cpp
@@ -0,0 +1,9 @@
+//
+// Created by Komodoman on 8/5/2015.
+//
+
+#include "PositionComponent.h"
+
+PositionComponent::PositionComponent() {
+
+}
---
+++ src/entity_system/components/PositionComponent.h
@@ -0,0 +1,16 @@
+//
+// Created by Komodoman on 8/5/2015.
+//
+
+#ifndef KOMODO_POSITIONCOMPONENT_H
+#define KOMODO_POSITIONCOMPONENT_H
+
+
+class PositionComponent {
+
+public:
+    PositionComponent();
+};
+
+
+#endif //KOMODO_POSITIONCOMPONENT_H
